703ee71a6f27add95e358fb65df06dc9211d80ed
raiden189 <raidengreat123@gmail.com>
2024-01-23 06:26:30 +0800

    add employee name and salary sort

--- EmployeeManagementSystemApp/EmployeeManagementSystemUI/Controllers/EmployeeListController.cs
+++ EmployeeManagementSystemApp/EmployeeManagementSystemUI/Controllers/EmployeeListController.cs
@@ -14,9 +14,32 @@ namespace EmployeeManagementSystemUI.Controllers
             _employeeRepository = employeeRepository;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder, string searchString)
         {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.SalarySortParm = String.IsNullOrEmpty(sortOrder) ? "salary_desc" : "";
+
             var employees = await _employeeRepository.GetEmployeesAsync();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                employees = employees.Where(w => w.Name.Contains(searchString))
+                    .ToList();
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    employees = employees.OrderByDescending(s => s.Name).ToList();
+                    break;
+                case "salary_desc":
+                    employees = employees.OrderByDescending(s => s.Salary).ToList();
+                    break;
+                default:
+                    employees = employees.OrderBy(s => s.Name).ToList();
+                    break;
+            }
+
             return View(employees);
         }
     }
